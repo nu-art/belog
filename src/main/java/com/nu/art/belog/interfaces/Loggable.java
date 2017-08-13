18d46d82a304f3bd52ea220e77404178e5078444
TacB0sS <adam.zehavi@nu-art-software.com>
2017-08-13 23:42:31 +0300

    Added Loggable interface.

---
+++ src/main/java/com/nu/art/belog/interfaces/Loggable.java
@@ -0,0 +1,12 @@
+package com.nu.art.belog.interfaces;
+
+import com.nu.art.belog.Logger;
+
+/**
+ * Created by tacb0ss on 12/08/2017.
+ */
+
+public interface Loggable {
+
+	void log(Logger logger);
+}
